93924cde3c5cfd92705d935715f18d4ce1a4a558
juan <jcamiloaviles@gmail.com>
2022-07-12 18:33:20 -0500

    Se agrega el proyecto

---
+++ src/main/java/org/diplomado/weapp/servelt/ParametrosGetServlet.java
@@ -0,0 +1,70 @@
+package org.diplomado.weapp.servelt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/parametros/url-get")
+public class ParametrosGetServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String saludo = req.getParameter("saludo");
+        String nombre = req.getParameter("nombre");
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println(" <meta charset=\"UTF-8\">");
+        out.println(" <title> Parametros Get de la url </title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1> Parametros Get de la url!</h1>");
+
+        if (saludo != null && nombre != null) {
+
+            out.println("          <h2> El saludo enviado es: " + saludo + " " + nombre + "</h2>");
+
+        } else if (saludo != null) {
+
+            out.println("     <h2> El saludo enviado es: " + saludo + "</h2>");
+
+        } else if (nombre != null) {
+
+            out.println("    <h2> hola mi nombre es " + nombre + "</h2>");
+
+        } else {
+
+            out.println("<h2> no se an pasado los parametros saludos ni nombre </h2>");
+        }
+
+
+        try {
+
+            int codigo = Integer.parseInt(req.getParameter("codigo"));
+            out.println("<h3> El codigo enviado es : " + codigo + "</h3>");
+
+        } catch ( NumberFormatException e) {
+
+            out.println("<h3> El codigo no se a enviado, es null </h3>");
+
+        }
+
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+
+
+}
